85597af7614915d0a3dc09e37397b2014695862d
Oleh PF <olehpf@gmail.com>
2024-05-31 22:54:27 +0300

    Updated configurations

--- config.js
+++ config.js
@@ -2,11 +2,12 @@
 font properties. Here's a breakdown of what each part of the code is doing: */
 
 const StyleDictionary = require('style-dictionary');
+const configStyleDictionary = require('./config.js');
 
 // Filter for color properties
 StyleDictionary.registerFilter({
   name: 'isColor',
-  matcher: function(token) {
+  matcher: function (token) {
     return token.path[0] === 'light' || token.path[0] === 'dark';
   }
 });
@@ -14,11 +15,10 @@ StyleDictionary.registerFilter({
 // Filter for font properties
 StyleDictionary.registerFilter({
   name: 'isFont',
-  matcher: function(token) {
+  matcher: function (token) {
     return token.path[0] === 'desktop' || token.path[0] === 'mobile';
   }
 });
 
 const styleDictionary = StyleDictionary.extend('config.json');
-
 styleDictionary.buildAllPlatforms();
