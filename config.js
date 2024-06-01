6a0ce43c95e35870c38c5639b6c11f040e33cd18
Oleh PF <olehpf@gmail.com>
2024-06-01 16:18:04 +0300

    Updates for configuration files and the scss variables

--- config.js
+++ config.js
@@ -1,38 +1,62 @@
 /* This JavaScript code snippet is using the Style Dictionary library to define filters for color and
 font properties. Here's a breakdown of what each part of the code is doing: */
 
-const StyleDictionary = require('style-dictionary');
-const configStyleDictionary = require('./config.js');
+const StyleDictionary = require("style-dictionary");
+const configStyleDictionary = require("./config.js");
 
 // Filter for color properties
 StyleDictionary.registerFilter({
-  name: 'isColor',
+  name: "isColor",
   matcher: function (token) {
-    return token.path[0] === 'light' || token.path[0] === 'dark';
-  }
+    return token.path[0] === "light" || token.path[0] === "dark";
+  },
 });
 
 // Filter for font properties
 StyleDictionary.registerFilter({
-  name: 'isFont',
+  name: "isFont",
   matcher: function (token) {
-    return token.path[0] === 'desktop' || token.path[0] === 'mobile';
-  }
+    return token.path[0] === "desktop" || token.path[0] === "mobile";
+  },
 });
 
 // Build the Camel Case name for the token
 StyleDictionary.registerTransform({
-  name: 'name/camel',
-  type: 'name',
-  transformer: function(token) {
-    return token.name.replace(/(\w)(\w*)/g, function(g0,g1,g2){return g1.toUpperCase() + g2.toLowerCase();});
-  }
+  name: "name/camel",
+  type: "name",
+  transformer: function (token) {
+    return token.name.replace(/(\w)(\w*)/g, function (g0, g1, g2) {
+      return g1.toUpperCase() + g2.toLowerCase();
+    });
+  },
 });
 
 StyleDictionary.registerTransformGroup({
-  name: 'scss',
-  transforms: ['attribute/cti', 'name/cti/camel', 'color/css', 'size/rem']
+  name: "scss",
+  transforms: ["attribute/cti", "name/cti/camel", "color/css", "size/rem"],
 });
 
-const styleDictionary = StyleDictionary.extend('config.json');
+// Register a custom format that includes the file header
+StyleDictionary.registerFormat({
+  name: "scss/variables",
+  formatter: function (dictionary, platform, file, config) {
+    const currentDate = new Date();
+    const fileHeader = [
+      `/** --------------------------------------------------------------------------`,
+      ` * The ${file.destination} file.`,
+      ` * Generated on: ${currentDate}`,
+      ` * russia is a terrorist state 💯💩 `,
+      ` ----------------------------------------------------------------------------- **/`,
+    ].join("\n");
+    const fileContent = dictionary.allProperties
+      .map(function (prop) {
+        return `$${prop.name}: ${prop.value};`;
+      })
+      .join("\n");
+    return `${fileHeader}\n\n${fileContent}`;
+  },
+});
+
+const styleDictionary = StyleDictionary.extend("config.json");
+
 styleDictionary.buildAllPlatforms();
